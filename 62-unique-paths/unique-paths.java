0c3e9aa2f18a7432e1ce9631a2a97b274b1b8048
Abhaychaudhary0007 <abhaychaudhary1234as@gmail.com>
2026-04-08 10:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.85%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int[][] dp;
+    public int helper(int m, int n,int[][]dp){
+        if(m==1||n==1){
+            return 1;
+        }
+            if(dp[m][n]!=0){
+                return dp[m][n];
+            }
+            else dp[m][n]=helper(m-1,n,dp)+helper(m,n-1,dp);
+            return dp[m][n];
+    }
+    public int uniquePaths(int m, int n) {
+        dp=new int[m+1][n+1];
+        return helper(m,n,dp);
+    }
+}
