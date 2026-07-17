e18cc096d18fde97ce1e996b885ec28ab30f7c37
Abhaychaudhary0007 <abhaychaudhary1234as@gmail.com>
2026-07-17 15:22:28 +0530

    Time: 19 ms (10.74%) | Memory: 47.1 MB (5.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        if(Arrays.equals(a,b)){
-            return true;
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> amap=new HashMap<>();
+        HashMap<Character,Integer> bmap=new HashMap<>();
+    
+    for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(amap.containsKey(ch)){
+            int freq=amap.get(ch);
+            amap.put(ch,freq+1);
         }
-   return false;
+        else amap.put(ch,1);
+    }
+    for(int i=0;i<t.length();i++){
+        char ha=t.charAt(i);
+        if(!amap.containsKey(ha)) return false;
+        
+        if(bmap.containsKey(ha)){
+            int freq=bmap.get(ha);
+            bmap.put(ha,freq+1);
+        }
+        else bmap.put(ha,1);
+            
+
+    }
+
+     for(char ele: bmap.keySet()){
+        if(amap.containsKey(ele)){
+            int afreq=amap.get(ele);
+            int bfreq=bmap.get(ele);
+            if(afreq!=bfreq){
+                return false;
+            }
+        }
+     }
+return true;
     }
 }
