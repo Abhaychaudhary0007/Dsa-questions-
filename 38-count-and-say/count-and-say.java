63707a354e9f9c3990e351f7bb732656e2d8707d
Abhaychaudhary0007 <abhaychaudhary1234as@gmail.com>
2026-07-23 11:43:29 +0530

    Time: 24 ms (5.17%) | Memory: 52.8 MB (5.29%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        String s=countAndSay(n-1);
+        String ans="";
+
+        int j=0; int i=0;
+        while(j<s.length()){
+            if(s.charAt(i)==s.charAt(j)) j++;
+
+            else{
+                int fre=j-i;
+                ans+=fre;
+                ans+=s.charAt(i);
+                i=j;
+            }
+        }
+       int fre=j-i;
+          ans+=fre;
+                ans+=s.charAt(i);
+
+                return ans;
+        
+    }
+}
