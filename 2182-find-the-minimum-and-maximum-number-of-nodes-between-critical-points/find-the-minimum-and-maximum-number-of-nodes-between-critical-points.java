31d9e4bbe84762c03cb52a974906c928849697a5
Abhaychaudhary0007 <abhaychaudhary1234as@gmail.com>
2026-08-06 02:15:21 +0530

    Time: 4 ms (100.00%) | Memory: 106.2 MB (56.94%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int [] ans=new int[2];
+        ans[0]=-1;
+        ans[1]=-1;
+        if(head==null || head.next==null || head.next.next==null) return ans;
+        ListNode a=head;
+        ListNode b=head.next;
+        ListNode c=head.next.next;
+        int idx=1;
+        int first=-1;
+        int last=-1;
+        int minsub=Integer.MAX_VALUE;
+        int sum=0;
+
+        while(c!=null){
+            if((b.val> a.val && b.val>c.val) || (b.val<a.val && b.val<c.val)){
+               if(first==-1){
+                first=idx;
+                last=idx;
+               }
+               else{
+                sum=idx-last;
+                last=idx;
+                minsub=Math.min(minsub,sum);
+               }
+               a = b;
+               b = c;
+               c = c.next;
+               idx++;
+            }
+            else{
+                a=b;
+                b=c;
+                c=c.next;
+               
+                 idx++;
+            }
+            
+
+            
+        }
+        if(first==-1 || first==last) return ans;
+        int max=last-first;
+        ans[0]=minsub;
+        ans[1]=max;
+
+        return ans;
+    }
+}
